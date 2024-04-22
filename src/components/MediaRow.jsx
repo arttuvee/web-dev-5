61da41876e13efdc1b90580a9518c83adedaa2c1
arttuvee <arttuvo@metropolia.fi>
2024-04-22 11:23:33 +0300

    Assignment 1 done

--- src/components/MediaRow.jsx
+++ src/components/MediaRow.jsx
@@ -6,17 +6,14 @@ const MediaRow = ({item}) => (
     <td>
       <img src={item.thumbnail} alt={item.title} />
     </td>
+    <td>{item.username}</td>
     <td>{item.title}</td>
     <td>{item.description}</td>
     <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
     <td>{item.filesize}</td>
     <td>{item.media_type}</td>
     <td>
-      <Link
-        to={`/media/${item.media_id}`}
-        state={{item}}
-        className="m-3 mt-10 rounded bg-blue-500 px-4 py-2 font-bold text-stone-100 hover:bg-blue-700"
-      >
+      <Link to={`/media/${item.media_id}`} state={{item}}>
         View
       </Link>
     </td>
