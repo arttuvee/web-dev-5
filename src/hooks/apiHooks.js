aa78780084ec8fbc5e29f47fd27d28645b062f21
arttuvee <arttuvo@metropolia.fi>
2024-04-22 13:08:16 +0300

    Assignment 2 done

---
+++ src/hooks/apiHooks.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { fetchData } from '../lib/fetchData';
+
+const useMedia = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const getMedia = async () => {
+    const mediaResult = await fetchData(import.meta.env.VITE_MEDIA_API + '/media');
+    const mediaWithUser = await Promise.all(
+      mediaResult.map(async (mediaItem) => {
+        const userResult = await fetchData(
+          import.meta.env.VITE_AUTH_API + '/users/' + mediaItem.user_id,
+        );
+        return {...mediaItem, username: userResult.username};
+      }),
+    );
+
+    console.log(mediaWithUser);
+    setMediaArray(mediaWithUser);
+  };
+
+  useEffect(() => {
+    getMedia();
+  }, []);
+
+  return mediaArray;
+};
+
+const useUser = () => {
+  const getUserById = async (id) => {
+    const userResult = await fetchData(
+      import.meta.env.VITE_AUTH_API + '/users/' + id,
+    );
+    return userResult;
+  };
+  return {getUserById}
+};
+
+export { useMedia, useUser };
